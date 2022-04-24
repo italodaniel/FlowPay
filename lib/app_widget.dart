8878aac290f5031821b63b9ea98c9323d7939c98
italodaniel <danielitallo@hotmail.com>
2022-04-23 23:04:11 -0300

    barcode capture controller

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -7,6 +7,14 @@ import 'modules/login/login_page.dart';
 import 'shared/themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
+
+  //AppWidget(){
+  //  SystemChrome.setPreferredOrientations({
+  //    DeviceOrientation.portraitDown,
+  //    DeviceOrientation.portraitUp,
+  //   });
+  //}
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
---
+++ lib/modules/barcode_scanner/barcode_scanner_status.dart
@@ -0,0 +1,35 @@
+import 'package:camera/camera.dart';
+
+class BarcodeScannerStatus {
+  final bool isCameraAvailable;
+  final String error;
+  final String barcode;
+
+  //dois argumentos - teste
+  CameraController? cameraController;
+
+  BarcodeScannerStatus({
+    this.isCameraAvailable = false,
+    this.error = "",
+    this.barcode = "",
+    CameraController? cameraController,
+  });
+
+  factory BarcodeScannerStatus.available(CameraController controller) =>
+      BarcodeScannerStatus(
+        isCameraAvailable: true,
+        cameraController: controller,
+      );
+
+  factory BarcodeScannerStatus.error(String message) =>
+      BarcodeScannerStatus(error: message);
+
+  factory BarcodeScannerStatus.barcode(String barcode) =>
+      BarcodeScannerStatus(error: barcode);
+
+  bool get showCamera => isCameraAvailable && error.isEmpty;
+
+  bool get hasError => error.isNotEmpty;
+
+  bool get hasBarcode => barcode.isNotEmpty;
+}
